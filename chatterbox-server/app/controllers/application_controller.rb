56041536c1b90b8cd18dbcc5101830b288ce974b
OyakMasu21 <akzaki17@gmail.com>
2023-03-02 01:00:07 +0300

    Completed lab

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,38 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+
+  get '/' do
+    "Hello World"
+  end
+
+  get '/messages' do
+    message = Message.all.order(created_at: :asc)
+    message.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
+
+
 end
